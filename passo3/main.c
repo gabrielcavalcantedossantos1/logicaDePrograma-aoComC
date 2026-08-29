f684624206bf40e8f90521e446029c468d0bf993
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-08-29 12:49:57 -0300

    testando o comando scanf

---
+++ passo3/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int idade;
+
+int main()
+{
+
+    printf("Digite a sua idade: ");
+    scanf(" %d", &idade);
+
+    printf("O usuario tem %d anos de idade", idade);
+
+    return 0;
+}
